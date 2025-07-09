fb8bb92ff96242fc96a9c0dd9388c00a8e303f57
ToanHuynh0201 <toanhuynhlilyiu@gmail.com>
2025-07-09 09:06:41 +0700

    Init, Login, Logout, Register

---
+++ app/(auth)/register.tsx
@@ -0,0 +1,136 @@
+import BackButton from "@/components/BackButton";
+import Button from "@/components/Button";
+import Input from "@/components/Input";
+import ScreenWrapper from "@/components/ScreenWrapper";
+import Typo from "@/components/Typo";
+import { colors, spacingX, spacingY } from "@/constants/theme";
+import { verticalScale } from "@/utils/styling";
+import { useRouter } from "expo-router";
+import * as Icons from "phosphor-react-native";
+import React, { useRef, useState } from "react";
+import { Alert, Pressable, StyleSheet, View } from "react-native";
+const Register = () => {
+  const router = useRouter();
+
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const nameRef = useRef("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = async () => {
+    if (!emailRef.current || !passwordRef.current || !nameRef.current) {
+      Alert.alert("Sign up", "Please fill all the fields");
+      return;
+    }
+
+    console.log("email", emailRef.current);
+    console.log("name", nameRef.current);
+    console.log("password", passwordRef.current);
+    console.log("Good to go");
+  };
+  return (
+    <ScreenWrapper>
+      <View style={styles.container}>
+        <BackButton iconSize={28} />
+
+        <View style={{ gap: 5, marginTop: spacingY._20 }}>
+          <Typo size={30} fontWeight={"800"}>
+            Let's,
+          </Typo>
+          <Typo size={30} fontWeight={"800"}>
+            Get Started
+          </Typo>
+        </View>
+
+        <View style={styles.form}>
+          <Typo size={16} color={colors.textLighter}>
+            Create an account to track your expenses
+          </Typo>
+          <Input
+            onChangeText={(value) => (nameRef.current = value)}
+            placeholder="Enter your name"
+            icon={
+              <Icons.User
+                size={verticalScale(26)}
+                color={colors.neutral300}
+                weight="fill"
+              />
+            }
+          />
+          <Input
+            onChangeText={(value) => (emailRef.current = value)}
+            placeholder="Enter your email"
+            icon={
+              <Icons.At
+                size={verticalScale(26)}
+                color={colors.neutral300}
+                weight="fill"
+              />
+            }
+          />
+          <Input
+            onChangeText={(value) => (passwordRef.current = value)}
+            secureTextEntry
+            placeholder="Enter your password"
+            icon={
+              <Icons.Lock
+                size={verticalScale(26)}
+                color={colors.neutral300}
+                weight="fill"
+              />
+            }
+          />
+
+          <Button loading={isLoading} onPress={handleSubmit}>
+            <Typo fontWeight={"700"} color={colors.black} size={21}>
+              Sign Up
+            </Typo>
+          </Button>
+        </View>
+
+        <View style={styles.footer}>
+          <Typo size={15}>Already have an account?</Typo>
+          <Pressable onPress={() => router.navigate("/(auth)/login")}>
+            <Typo size={15} fontWeight={"700"} color={colors.primary}>
+              Login
+            </Typo>
+          </Pressable>
+        </View>
+      </View>
+    </ScreenWrapper>
+  );
+};
+
+export default Register;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: spacingX._30,
+    paddingHorizontal: spacingX._20,
+  },
+  welcomeText: {
+    fontSize: verticalScale(20),
+    fontWeight: "bold",
+    color: colors.text,
+  },
+  form: {
+    gap: spacingY._20,
+  },
+  forgotPassword: {
+    textAlign: "right",
+    fontWeight: "500",
+    color: colors.text,
+  },
+  footer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 5,
+  },
+  footerText: {
+    textAlign: "center",
+    color: colors.text,
+    fontSize: verticalScale(15),
+  },
+});
